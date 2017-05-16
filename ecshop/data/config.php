9fc7e72ccd921f899ac2744685ab5b8584e6db5d
puff <puffan@sina.com>
2017-05-16 23:06:54 +0800

    ecshop src code 3.6

---
+++ ecshop/data/config.php
@@ -0,0 +1,37 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "caches";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "root";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "PRC";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '');
+
+define('STORE_KEY','753e8a2cac6fe6cd82ed5b2d5da3559b');
+
+?>
